b65ec4d36d8557098c3c44ef3d7377008735e2aa
cabanesc <38859979+cabanesc@users.noreply.github.com>
2025-09-26 17:01:16 +0200

    clean comments

--- ow_calibration_bulk_processing.m
+++ ow_calibration_bulk_processing.m
@@ -4,7 +4,7 @@
 % - Output plots are saved with the suffix '_ctd' or '_argo'. 
 % - Plots are not displayed interactively but directly saved to
 % files (if goHeadless = true)
-%
+% 
 % This workflow requires creating two files from the original wmo_boxes.mat :
 %   • wmo_boxes_argo.mat : to use only Argo reference data (columns 2 and 3
 %   of la_wmo_boxes array are set to all 0s). 
@@ -27,7 +27,7 @@
 % - addapt the call to the function plot_diagnostics_ow.m
 % - simplify the handling of mapping files avoiding redundant copies:
 %  mapped files are saved in ./float_mapped/argo/ and ./float_mapped/ctd/
-%  directories
+%  directories. Changes are not tracked for readabitliy
 
 
 % float areas
@@ -73,23 +73,10 @@ for wmoid = wmoids
     fprintf('%s Working on %d\n', datestr(now), wmoid);
     %fprintf('%s\n', lo_system_configuration.CONFIG_WMO_BOXES);
 
-%[CC20250918/]      
-%     % the default OWC mapped name
-%     genericMappedName = sprintf('%s/data/float_mapped/map_%d.mat', OWCdir, wmoid);
-%     % CTD name we want it to have
-%     ctdMappedName = sprintf('%s/data/float_mapped/map_%d_ctd.mat', OWCdir, wmoid);
-%     % ARGO name we want it to have
-%     argoMappedName = sprintf('%s/data/float_mapped/map_%d_argo.mat', OWCdir, wmoid);
-%[/CC20250918] 
-
 
     % sometimes we want to use the historical CTD data
     if(doCTD)
-%[CC20250918/]             
-%         % swap argo for ctd comparison
-%         lo_system_configuration.CONFIG_WMO_BOXES = ...
-%             strrep(lo_system_configuration.CONFIG_WMO_BOXES, 'argo',
-%             'ctd'); % problem if CONFIG_WMO_BOXES is wmo_boxes.mat 
+
         lo_system_configuration.CONFIG_WMO_BOXES = 'wmo_boxes_ctd.mat'; 
         la_wmo_boxes_file=strcat( lo_system_configuration.CONFIG_DIRECTORY, lo_system_configuration.CONFIG_WMO_BOXES);
 
@@ -101,61 +88,6 @@ for wmoid = wmoids
         disp(lo_system_configuration)
         ctdMappedName = fullfile( lo_system_configuration.FLOAT_MAPPED_DIRECTORY, flt_name, strcat( lo_system_configuration.FLOAT_MAPPED_PREFIX, flt_name, lo_system_configuration.FLOAT_MAPPED_POSTFIX ) );
 
-%[/CC20250918] 
-%[CC20250918/] 
-%         % check on mapped file status
-%         if(exist(ctdMappedName, 'file'))
-%             % we have run CTD before so lets copy it to the working/generic
-%             % name - copyfile(src, dest)
-%             % but 1st check if _argo exists.
-%             if(exist(argoMappedName, 'file'))
-%                 % yes, so no need to backup
-%                 fprintf('keeping existing _argo file during ctd comparison\n');
-%             elseif (exist(genericMappedName, 'file')) % if(exist(argoMappedName, 'file'))
-%                 % rename generic as _argo ie backup
-%                 mvStat = movefile(genericMappedName, argoMappedName);
-%                 % if a move error happens go back
-%                 if(~mvStat)
-%                     fprintf('error whilst renaming generic to %s\n', argoMappedName);
-%                     % now return to the previous dir
-%                     cd(currentDir);
-%                     return
-%                 end % if(~mvStat)
-%                 fprintf('backing up generic file as %s during ctd comparison\n', argoMappedName);
-%             end % if(exist(argoMappedName, 'file'))
-%             % ow copy the _ctd file to generic
-%             mvStat = copyfile(ctdMappedName, genericMappedName);
-%             % if a move error happens go back
-%             if(~mvStat)
-%                 fprintf('error whilst copying _ctd to %s\n', genericMappedName);
-%                 % now return to the previous dir
-%                 cd(currentDir);
-%                 return
-%             end % if(~mvStat)
-%             fprintf('using %s as generic file\n', ctdMappedName);
-%         else % if(exist(ctdMappedName, 'file'))
-%             % ctd does not yet exist, so assume generic file is the _argo one
-%             % which needs to be backed up to _argo
-%             % 1st check if it exists
-%             if(exist(genericMappedName, 'file'))
-%                 mvStat = movefile(genericMappedName, argoMappedName);
-%                 % if a move error happens go back
-%                 if(~mvStat)
-%                     fprintf('error whilst renaming generic to %s\n', argoMappedName);
-%                     % now return to the previous dir
-%                     cd(currentDir);
-%                     return
-%                 end % if(~mvStat)
-%                 fprintf('backing up generic file as %s during ctd comparison\n', argoMappedName);
-%             else % if(exist(genericMappedName, 'file'))
-%                 % does not exist let it create the generic
-%                 fprintf('no existing generic file during ctd comparison\n');
-%             end % if(exist(genericMappedName, 'file'))
-%         end % if(exist(ctdMappedName, 'file'))
-%         % at this point the generic mapped name should either be the _ctd one or
-%         % none at all, 1st ctd run
-%[/CC20250918]     
-
         % now run it again
         update_salinity_mapping( flt_dir, flt_name, lo_system_configuration );
         set_calseries( flt_dir, flt_name, lo_system_configuration );
@@ -164,32 +96,12 @@ for wmoid = wmoids
         %plot_diagnostics_ow_CSIRO( flt_dir, flt_name, lo_system_configuration, goHeadless );
         plot_diagnostics_ow( flt_dir, flt_name, lo_system_configuration,'pltFileType',pltFileType,'appendRef','_ctd','goHeadless',goHeadless);
 % [/CC20250918]  
-% [CC20250918/] 
-%         % swap ctd back to argo
-%         lo_system_configuration.CONFIG_WMO_BOXES = ...
-%             strrep(lo_system_configuration.CONFIG_WMO_BOXES, 'ctd', 'argo');
-%         
-%         % backup the _ctd mapped data 
-%         mvStat = movefile(genericMappedName, ctdMappedName);
-%         % if a move error happens go back
-%         if(~mvStat)
-%             fprintf('error whilst renaming to %s\n', ctdMappedName);
-%             % now return to the previous dir
-%             cd(currentDir);
-%             return
-%         end % if(~mvStat)
-%         fprintf('generic file saved as %s during ctd comparison\n', ctdMappedName);
          fprintf('mapped file saved as %s during ctd comparison\n', ctdMappedName);
-% [/CC20250918] 
 
     end % if(doCTD)
     
     % let's use the ARGO boxes data
     
-% [CC20250918/]
-%     % swap ctd to argo, just to be safe
-%     lo_system_configuration.CONFIG_WMO_BOXES = ...
-%         strrep(lo_system_configuration.CONFIG_WMO_BOXES, 'ctd', 'argo');
     lo_system_configuration.CONFIG_WMO_BOXES = 'wmo_boxes_argo.mat';
     la_wmo_boxes_file=strcat( lo_system_configuration.CONFIG_DIRECTORY, lo_system_configuration.CONFIG_WMO_BOXES);
 
@@ -201,29 +113,6 @@ for wmoid = wmoids
     disp(lo_system_configuration)
     argoMappedName = fullfile(lo_system_configuration.FLOAT_MAPPED_DIRECTORY, flt_name, strcat(lo_system_configuration.FLOAT_MAPPED_PREFIX, flt_name, lo_system_configuration.FLOAT_MAPPED_POSTFIX ) );
 
-% [/CC20250918] 
-
-% [CC20250918/]
-%     % check on mapped file status
-%     if(exist(argoMappedName, 'file'))
-%         % we have an _argo, copy it to the working/generic name - 
-%         % copyfile(src, dest)
-%         mvStat = copyfile(argoMappedName, genericMappedName);
-%         % if a move error happens go back
-%         if(~mvStat)
-%             fprintf('error whilst copying _argo to %s\n', genericMappedName);
-%             % now return to the previous dir
-%             cd(currentDir);
-%             return
-%         end % if(~mvStat)
-%         fprintf('using %s as generic file during argo comparison\n', argoMappedName);
-%     else % if(exist(argoMappedName, 'file'))
-%         % does not exist so the generic is likely the _argo data
-%         % NOTE: it could be the generic or could be the _ctd but let's be
-%         % brave
-%         fprintf('using existing generic file during argo comparison\n');
-%     end % if(exist(argoMappedName, 'file'))
-% [/CC20250918] 
 
     update_salinity_mapping( flt_dir, flt_name, lo_system_configuration );
     set_calseries( flt_dir, flt_name, lo_system_configuration );
@@ -233,20 +122,8 @@ for wmoid = wmoids
     plot_diagnostics_ow( flt_dir, flt_name, lo_system_configuration,'pltFileType',pltFileType,'appendRef','_argo','goHeadless',goHeadless);
 %[CC20250618/]
 
-%[CC20250618/]
-%     % backup the _argo mapped data and keep generic as _argo
-%     mvStat = copyfile(genericMappedName, argoMappedName);
-%     % if a move error happens go back
-%     if(~mvStat)
-%         fprintf('error whilst copying to %s\n', argoMappedName);
-%         % now return to the previous dir
-%         cd(currentDir);
-%         return
-%     end % if(~mvStat)
-%     fprintf('generic file saved as %s during argo comparison\n', argoMappedName);
-    fprintf('mapped file saved as %s during argo comparison\n', argoMappedName);
 
-    %[CC20250618/]    
+    fprintf('mapped file saved as %s during argo comparison\n', argoMappedName);
 
 end % wmoid
 
